c0a9c0ced28ecf5ee8624dcef421063cd637f13c
Ile Eftimov <ileeftimov@gmail.com>
2014-09-12 01:25:52 +0200

    add rainbow gem; formatting to repo_stats

--- ghit.gemspec
+++ ghit.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'github_api', '~> 0.12'
+  spec.add_dependency 'rainbow', '~> 2.0'
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake", "~> 10.3"
   spec.add_development_dependency 'rspec', "~> 3.1"
--- lib/ghit/repo_statistics.rb
+++ lib/ghit/repo_statistics.rb
@@ -3,11 +3,13 @@ module Ghit
     def initialize
       globals = Ghit::Globals.new
       code_frequency = Github::Client::Repos::Statistics.new.code_frequency(user: globals.author, repo: globals.repository).body
+      puts "From:\tAdditions\tDeletions"
+      puts "---------------------------"
       code_frequency.each do |date, additions, deletions|
-        puts date
-        puts additions 
-        puts deletions
+        date = DateTime.strptime(date.to_s, "%s")
+        puts "#{date.to_date.to_s}\t#{additions}\t#{deletions}"
       end
+      puts "---------------------------"
     end
   end
 end
