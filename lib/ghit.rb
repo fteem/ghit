eb1ac0876219751dc8f2dba3cf59738402142bc1
Ile Eftimov <ileeftimov@gmail.com>
2014-09-15 00:31:31 +0200

    fix naming bug in ghit.rb

--- lib/ghit.rb
+++ lib/ghit.rb
@@ -4,7 +4,7 @@ require 'rainbow'
 
 module Ghit
   autoload :Globals,         'ghit/globals'
-  autoload :HttpGlobals,     'ghit/http_globals'
+  autoload :HttpsGlobals,    'ghit/https_globals'
   autoload :SshGlobals,      'ghit/ssh_globals'
   autoload :RepoLocator,     'ghit/repo_locator'
   autoload :Errors,          'ghit/errors'
